4db55fa332677d8b2e9d8725e6b50ce105201849
nafish-alam <nafish.alam@madgicaltechdom.com>
2023-11-25 18:03:31 +0530

    CU-86797n3p9 - Waeviate Data Management System: A Robust and Scalable Solution for Secure Storage and Retrieval

---
+++ jayant bhaiya/creatingVactor.py
@@ -0,0 +1,4 @@
+from textToVactor import convertTextToVectors
+text="where is Mandideep?"
+vactor=convertTextToVectors(text)
+print("Vactor :",vactor)
---
+++ jayant bhaiya/vactorSearch.py
@@ -0,0 +1,6 @@
+from searchData import searchData
+
+
+question="How was the reception of the Honorable Prime Minister Narendra Modi on his arrival in Jharkhand?"
+getdata=searchData(question,1)
+print("getdata",getdata)
