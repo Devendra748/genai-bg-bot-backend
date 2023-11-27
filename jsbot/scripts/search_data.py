a7a952d2e54cc1df67ce769d53762a4645d1e5c9
nafish-alam <nafish.alam@madgicaltechdom.com>
2023-11-27 19:40:12 +0530

    CU-86797vnkh_Restructure-the-FastAPI-code-using-standard-boilerplate-code_MD-NAFISH-ALAM-

---
+++ jsbot/scripts/search_data.py
@@ -0,0 +1,28 @@
+import os
+import weaviate
+from dotenv import load_dotenv
+from create_vector import convertTextToVectors
+
+load_dotenv()
+WEAVIATE_CLUSTER_URL = os.getenv("WEAVIATE_URL")
+WEAVIATE_CLASS_NAME = "bot"
+
+
+# Weaviate configuration and Initialize the Weaviate client
+client = weaviate.Client(url=WEAVIATE_CLUSTER_URL)
+def searchData(question,number):
+    vectors = convertTextToVectors(question)
+   
+    response = (
+        client.query
+        .get(f"{WEAVIATE_CLASS_NAME}", ["question_English","answer_English","question_Hindi","answer_Hindi"])
+        .with_near_vector({
+            "vector": vectors[0]})
+        # .with_limit(5)
+        .with_limit(number)
+       .with_additional(["distance"])
+    .do()
+    
+    )
+  
+    return(response)
