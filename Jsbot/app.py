8b86cf9e3b5dc960295e20e380c6e34820ba7561
Vivek <vkvashistha@gmail.com>
2023-12-14 06:10:03 +0530

    Azure  endpoint added

--- Jsbot/app.py
+++ Jsbot/app.py
@@ -21,13 +21,15 @@ from llama_index import (
     ServiceContext,
     load_index_from_storage,
 )
-from llama_index.llms import AzureOpenAI
 
 from dotenv import load_dotenv
 import json
 import logging  # Add this import statement
 import sys
 import os
+from llama_index.llms import AzureOpenAI
+from llama_index.embeddings import AzureOpenAIEmbedding
+from llama_index import set_global_service_context
 
 
 
@@ -62,7 +64,27 @@ class SearchDataPayload(BaseModel):
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+llm = AzureOpenAI(
+    engine=os.getenv("OPENAI_AZURE_GENERATION_MODEL_DEPLOYMENT_NAME"),
+    model=os.getenv("OPENAI_AZURE_GENERATION_MODEL_NAME"),
+    deployment_name=os.getenv("OPENAI_AZURE_GENERATION_MODEL_DEPLOYMENT_NAME"),
+    temperature=0.0,
+    azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT"),
+    api_key=os.getenv("OPENAI_AZURE_API_KEY"),
+    api_version=os.getenv("OPENAI_AZURE_API_VERSION"),
+)
+
+embed_model = AzureOpenAIEmbedding(
+    model=os.getenv("OPENAI_AZURE_EMBEDDING_MODEL_NAME"),
+    deployment_name=os.getenv("OPENAI_AZURE_EMBEDDING_MODEL_DEPLOYMENT_NAME"),
+    api_key=os.getenv("OPENAI_AZURE_API_KEY"),
+    azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT"),
+    api_version=os.getenv("OPENAI_AZURE_API_VERSION"),
+)
+
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
+set_global_service_context(service_context)
 
 # Load or create the llama_index
 if not os.path.exists("./storage"):
