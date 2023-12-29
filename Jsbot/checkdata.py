f23d533e345dfffceafa7b569599366185fd628c
Devendra748 <choukikerdevendra86@gmail.com>
2023-12-29 19:34:10 +0530

    initial commit

--- Jsbot/checkdata.py
+++ Jsbot/checkdata.py
@@ -5,10 +5,10 @@ import os
 import json
 import torch.nn.functional as F
 WEAVIATE_CLUSTER_URL = os.getenv("WEAVIATE_URL")
-client = weaviate.Client(url="http://ec2-3-111-45-171.ap-south-1.compute.amazonaws.com:8080")
+client = weaviate.Client(url="http://ec2-43-205-131-196.ap-south-1.compute.amazonaws.com:8080")
 
-classname = "LlamaIndexEnglish"
+classname = "Bgbot"
 
 all_objects = client.data_object.get(class_name=f"{classname}")
-with open('/home/nafish/Desktop/test/jayant-sinha-chatbot/Jsbot/Fetch_data.json', 'w', encoding='utf-8') as json_file:
+with open('/home/h/Documents/NewTestingWorkBGDemo/jayant-sinha-chatbot/Jsbot/Fetch_data.json', 'w', encoding='utf-8') as json_file:
     json.dump(all_objects, json_file, ensure_ascii=False, indent=4)
