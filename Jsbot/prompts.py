3925e21561e434cbc9c30a37be97963d202ee98e
Devendra748 <109965832+Devendra748@users.noreply.github.com>
2024-01-04 16:44:08 +0530

    Update prompts.py

--- Jsbot/prompts.py
+++ Jsbot/prompts.py
@@ -6,7 +6,20 @@ def create_custom_prompt(language: str) -> PromptTemplate:
 Given a conversation (between Human and Assistant) and a follow up message from Human, \
 rewrite the message to be a standalone question that captures all relevant context \
 from the conversation.Also the standalone question should be prefixed with 'Answer in {language}.
-
+Don't repeat the line or words which you already mentioned answer.
+Use punctuation when provide answer and seperate the lines with punctuation.
+use [
+पूर्ण विराम (Full Stop) - । or .
+कमा (Comma) - ,
+विसर्ग (Colon) - :
+अंतरक्षेप (Semicolon) - ;
+विसर्जन (Exclamation Mark) - !
+प्रश्न चिन्ह (Question Mark) - ?
+अनुबन्धक (Quotation Marks) - " "
+अंध विसर्जन (Ellipsis) - ...
+एन डैश (En Dash) - -
+एम डैश (Em Dash) - — ]
+punctuations.
 <Chat History>
 {{chat_history}}
 
